c30c41668f340b77183e021765d47c4f5bb29870
LogiqueClergyman <adityavishwanathanverma@gmail.com>
2023-01-22 22:32:08 +0530

    Second2

---
+++ src/A1/All_Indices_Problem.java
@@ -0,0 +1,26 @@
+package A1;
+import java.util.*;
+public class All_Indices_Problem {
+    public static void main(String[]args) {
+        // Your Code Here
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[]arr = new int[size];
+        for(int i = 0 ; i<size ; i++){
+            arr[i] = sc.nextInt();
+        }
+        int t = sc.nextInt();
+        helper(arr, 0, t);
+    }
+    public static void helper(int[]arr, int i, int target){
+        if(i==arr.length){
+            return;
+        }
+
+        if(arr[i] == target){
+            System.out.print(i + " ");
+            // helper(arr, i+1, target);
+        }
+        helper(arr,i+1,target);
+    }
+}
